dbb9c39ab1e39d4f04b16130ee36245fbc306349
Liam Harbec <liam.harbec@gmail.com>
2021-02-27 16:28:19 -0800

    Initialized first class'

---
+++ assignment2/src/assignment2/Chatbot.java
@@ -0,0 +1,12 @@
+package assignment2;
+
+public class Chatbot {
+	//This is the Gym Help assistant
+	//This chat assistant can chat about a variety of gym related
+	//topics and is designed to help you get to know our gym (Name to be announced)
+	//Potential topics:
+	//Gym opening and closing time, types of equipment, 
+	//recommended starter exercises (on a per equipment basis)
+	//available trainers (their times, specializations, etc)
+
+}
